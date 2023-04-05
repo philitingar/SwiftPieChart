bb580260ad1839ae25edb8d25699bc19b8bef512
Timea Bartha <timi@Timeas-Air.lan>
2023-04-05 19:37:16 +0200

    Made it so the background is transparent without affecting the inner circle

--- Sources/SwiftPieChart/PieChartView.swift
+++ Sources/SwiftPieChart/PieChartView.swift
@@ -82,7 +82,6 @@ public struct PieChartView: View {
                             }
                     )
                     Circle()
-                        .fill(self.backgroundColor)
                         .frame(width: widthFraction * geometry.size.width * innerRadiusFraction, height: widthFraction * geometry.size.width * innerRadiusFraction)
                     
                     VStack {
@@ -96,7 +95,6 @@ public struct PieChartView: View {
                 }
                 PieChartRows(colors: self.colors, names: self.names, values: self.values.map { self.formatter($0) }, percents: self.values.map { String(format: "%.0f%%", $0 * 100 / self.values.reduce(0, +)) })
             }
-            .background(self.backgroundColor)
             .foregroundColor(Color.white)
         }
     }
